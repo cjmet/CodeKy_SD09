1b2d3adfbfa400a698a8ac41e15e6f5000b7d9ec
CJ Metcalfe <khaaiev@gmail.com>
2024-02-05 23:51:21 -0500

    Broken

--- DataLibrary/OrderRepository.cs
+++ DataLibrary/OrderRepository.cs
@@ -23,13 +23,14 @@ namespace DataLibrary
 
         public void AddOrder(OrderEntity order)
         {
-
+            //_dbContext.ChangeTracker.Clear();
             Console.WriteLine("Adding Order to Database");
             Console.WriteLine($"Tracking: {_dbContext.Orders.Entry(order).State}");
             Console.WriteLine($"Order ID: {order.Id}");
             Console.WriteLine($"Order Date: {order.OrderDate}");
             Console.WriteLine($"Order Products: {order.Products.Count}");
             Console.WriteLine();
+
             foreach (var product in order.Products)
             {
                 Console.WriteLine($"Product ID: {product.Id}");
@@ -41,7 +42,7 @@ namespace DataLibrary
             _dbContext.ChangeTracker.Clear();
             foreach (var product in order.Products)
                 if (_dbContext.Products.Entry(product).State == EntityState.Detached)
-                    _dbContext.Products.Attach(product); // cjm 
+                    _dbContext.Products.Attach(product); // cjm  
 
             _dbContext.Orders.Add(order);
             _dbContext.SaveChanges();       // cjm
